af776d1f1c053b07c3878b283250301beb4b067a
Lisa Folkerson <lisafolkerson@gmail.com>
2015-03-24 16:27:24 -0400

    strip inline styles, murder out placeholder text

--- assets/scripts/murdered.js
+++ assets/scripts/murdered.js
@@ -1,19 +1,28 @@
 $.fn.murdered ({
-	//set interval to run code every so often
-	setInterval(function(){
+		//strip all inline styles
+		$('*').removeAttr('style');
 
-	//turn every element black
-	$( '*' ).css({ "backgroundImage":"none", "backgroundColor":"black", "color":"#333", "textShadow":"0 0 0 black", "border":"1px solid black", "boxShadow":"0 0 0 black","border-color": "black", "fill":"black" });
-	//turn every image greyscale and semi-opaque
-	$('img').css({"-webkit-filter":"grayscale()", "opacity":"0.4"});
-	$('img').css({"position":"relative"});
-	$('img').after('<div style= "background-color:black; opacity:0.8; position:absolute; top:0; left:0; width:100%; height:100%;">');
-	//delete h1
-	$( 'h1' ).
-	//append text "lights out for earth day"
+		//set interval to run code every so often
+		setInterval(function(){
+
+		//turn every element black
+		$( '*, *:before, *:after' ).css({ "backgroundImage":"none", "backgroundColor":"black", "color":"#333", "textShadow":"0 0 0 black", "border":"1px solid black", "boxShadow":"0 0 0 black","border-color": "black", "fill":"black" });
+
+		//turn every image greyscale and semi-opaque
+		$('img').css({"-webkit-filter":"grayscale()", "opacity":"0.4"});
+		$('*').css({"position":"relative"});
+		$('*').after('<div style= "background-color:black; opacity:0.8; position:absolute; top:0; left:0; width:100%; height:100%;">');
 
 
-	
-	}, 1000); //sets interval, end setInterval
 
+		//change placeholder text to black
+		$('*::-webkit-input-placeholder, *:-moz-placeholder, *::-moz-placeholder, *:-ms-input-placeholder').css({ "backgroundImage":"none", "backgroundColor":"black", "color":"#333" });
+
+		//delete h1
+		$( 'h1' ).text(" ");
+
+		//append text "lights out for earth day"
+
+		
+		}, 1000); //sets interval, end setInterval
 }); //end murdered function
