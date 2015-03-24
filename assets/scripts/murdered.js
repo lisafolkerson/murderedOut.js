8a5d7171deabd7f201afbabb41af32e207e5c082
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-03-24 15:25:18 -0400

    dat overlay do

--- assets/scripts/murdered.js
+++ assets/scripts/murdered.js
@@ -3,7 +3,8 @@ $.fn.murdered ({
 	$( '*' ).css({ "backgroundImage":"none", "backgroundColor":"black", "color":"#333", "textShadow":"0 0 0 black", "border":"1px solid black", "boxShadow":"0 0 0 black","border-color": "black", "fill":"black" });
 	//turn every image greyscale and semi-opaque
 	// $('img').css({"-webkit-filter":"grayscale()", "opacity":"0.4"});
-	$('img').after()
+	$('img').css({"position":"relative"});
+	$('img').after('<div style= "background-color:black; opacity:0.5; position:absolute; top:0; left:0; width:100%; height:100%;">');
 	//delete h1
 	$( 'h1' ).
 	//append text "lights out for earth day"
