4be6764d87d70ca3a5c591bc89563fd990c64638
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-03-24 15:34:59 -0400

    some merge buzziness

--- assets/scripts/murdered.js
+++ assets/scripts/murdered.js
@@ -2,7 +2,7 @@ $.fn.murdered ({
 	//turn every element black
 	$( '*' ).css({ "backgroundImage":"none", "backgroundColor":"black", "color":"#333", "textShadow":"0 0 0 black", "border":"1px solid black", "boxShadow":"0 0 0 black","border-color": "black", "fill":"black" });
 	//turn every image greyscale and semi-opaque
-	// $('img').css({"-webkit-filter":"grayscale()", "opacity":"0.4"});
+	$('img').css({"-webkit-filter":"grayscale()", "opacity":"0.4"});
 	$('img').css({"position":"relative"});
 	$('img').after('<div style= "background-color:black; opacity:0.5; position:absolute; top:0; left:0; width:100%; height:100%;">');
 	//delete h1
