03e7a4ed5c7443688d34cc12270541e8ac09dbd9
Lisa Folkerson <lisafolkerson@gmail.com>
2015-03-24 15:12:44 -0400

    .after method

--- assets/scripts/murdered.js
+++ assets/scripts/murdered.js
@@ -3,7 +3,7 @@ $.fn.murdered ({
 	$( '*' ).css({ "backgroundImage":"none", "backgroundColor":"black", "color":"#333", "textShadow":"0 0 0 black", "border":"1px solid black", "boxShadow":"0 0 0 black","border-color": "black", "fill":"black" });
 	//turn every image greyscale and semi-opaque
 	// $('img').css({"-webkit-filter":"grayscale()", "opacity":"0.4"});
-	
+	$('img').after()
 	//delete h1
 	$( 'h1' ).
 	//append text "lights out for earth day"
