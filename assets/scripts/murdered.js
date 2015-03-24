e8b94a11950fcba64ed04c1c708bd43719b21d9a
Lisa Folkerson <lisafolkerson@gmail.com>
2015-03-23 22:02:09 -0400

    added jquery to turn things black

--- assets/scripts/murdered.js
+++ assets/scripts/murdered.js
@@ -1,4 +1,8 @@
 $.fn.murdered ({
-	$('*').css([ '"backgroundColor":"black", "color":"black", "' ]);
-	$('img').css([ '' ]);
+	//turn every element black
+	$( '*' ).css({ "backgroundColor":"black", "color":"#464646", "textShadow":"0 0 0 black", "border":"1px solid black", "boxShadow":"0 0 0 black","border-color": "black" });
+	//turn every image greyscale and semi-opaque
+	$( 'img' ).css({"filter" : "opacity(0.5)", "filter":"greyscale(100%)"});
+
+	$( 'h1' ).
 });
